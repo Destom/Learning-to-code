5b558f02090f2fe62e30950c082aa7a235105fd2
Destom <Destom@UW.COM>
2021-04-12 19:42:30 +0100

    adding db functions

--- db_testing/dbmod.py
+++ db_testing/dbmod.py
@@ -1,11 +1,17 @@
 import sqlite3 as lite
 import sys
 
-con = None
+
+def connect(db='objects.db'):
+    con = None
+    try:
+        con = lite.connect(db)
+    except:
+        print(lite.Error)
+    return con
 
 def test_connection():
-    print('testing connection')
-    con = lite.connect('objects.db') 
+    con = connect()
     cur = con.cursor()
     cur.execute('SELECT SQLITE_VERSION()')
     data = cur.fetchone()
@@ -13,15 +19,66 @@ def test_connection():
     con.close()
 
 def show_tables():
-    print('attempting to list tables')
-    con = lite.connect('objects.db')
+    con = connect()
     cur = con.cursor()
-    cur.execute('SHOW TABLES')
+    cur.execute('SELECT name FROM sqlite_master')
+    data = cur.fetchall()
+    con.close()
+    return (data)
+
+def check_for_table(name):
+    con = connect()
+    cur = con.cursor()
+    cur.execute(f'''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{name}' ''')
+    if cur.fetchone()[0]==1:
+        return 0
+    else:
+        return 1
+    con.close()
 
 def create_table(name,fields):
-    print(f'creating table: {name}')
-    print(f'Creating fileds: {fields}')
-    con = lite.connect('objects.db')
+    print(f'checking for table: {name}')
+    if check_for_table(name) == 1:
+        con = connect()
+        cur = con.cursor()
+        cur.execute(f'create table {name} ({fields})')
+        con.close()
+        return 0
+    else:
+        return 1
+
+def delete_table(name):
+    if check_for_table(name) == 0:
+        con = connect()
+        cur = con.cursor()
+        cur.execute(f'''DROP TABLE '{name}' ''')
+        return 0
+    else :
+        return 1
+
+def show_fields(table):
+    con = connect()
     cur = con.cursor()
-    cur.execute(f'create table {name} ({fields})')
+    if check_for_table(table) == 0:
+        cur.execute(f'''PRAGMA table_info({table})''')
+        data = cur.fetchall()
+        fields=[]
+        for i in data:
+            fields.append(i[1])
+        return(fields)
     con.close()
+
+def show_table(table):
+    con = connect()
+    cur = con.cursor()
+    if check_for_table(table) == 0:
+        cur.execute(f'''SELECT * FROM {table} ''')
+        return cur.fetchall()
+    con.close()
+
+def add_row(table,fields,values):
+    con = connect()
+    cur = con.cursor()
+    if check_for_table(table) == 0:
+        cur.execute(f'''INSERT INTO {table}{fields} VALUES {values}''')
+        con.commit()
--- db_testing/testing.py
+++ db_testing/testing.py
@@ -6,18 +6,31 @@ print('''welcome to the test environment, please select your test''')
 
 while choice != 'x':
     choice = input('''
-    1) test the db connection
-    2) show table list
+    1) Find table
+    2) Show table list
     3) Create table
+    4) Remove table
+    5) View table
+    6) add data
     x) exit the test lab
     Please make your selection: ''')
     if choice == '1':
-        dbmod.test_connection()
+        table = input('what is the table name? ')
+        print (dbmod.check_for_table(table))
     elif choice == '2':
-        dbmod.show_tables()
+        print(dbmod.show_tables())
     elif choice == '3':
         table = input('what is the table name? ')
         fields = input ('what are the fields(sepperated by a comma)? ')
-        dbmod.create_table(table,fields)
+        print(dbmod.create_table(table,fields))
+    elif choice == '4':
+        table = input('what table do you want to remove? ')
+        print(dbmod.delete_table(table))
+    elif choice == '5':
+        table = input('what table would you like to invesitgate? ')
+        print(dbmod.show_fields(table))
+        print(dbmod.show_table(table))
+    elif choice == '6':
+        dbmod.add_row('batman','(this,that)', ('baterang','grappeling-hook'))
     elif choice == 'x':
         pass
